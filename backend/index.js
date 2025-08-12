d74d4d62bff7e49c9ecf9789f346ba017e95efc1
abdelhalim <abdelhalim@gmail.com>
2025-08-12 12:12:28 +0300

    All crud opreations have made successfully

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,18 @@ app.get('/book' , async (req , res) => {
     }
 })
 
+//route to get specific book for a client by id 
+app.get('/book/:id' , async (req , res) => {
+    try {
+        const {id} = req.params
+        const sbook = await Book.findById(id);
+        return res.status(200).json(sbook)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({messsage: error.message})
+    }
+})
+
 app.post('/book' , async (req , res) => {
     try {
         if (
@@ -48,6 +60,45 @@ app.post('/book' , async (req , res) => {
     } catch (error) {
         console.log(error.message)
         res.status(500).send({message : error.message})
+    }
+})
+
+//route for update a specific book using its id
+app.put('/book/:id' , async (req , res) => {
+    try {
+        const {id} = req.params
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send("All fields are required : title | author | publishYear")
+        }
+        const result = await Book.findByIdAndUpdate(id , req.body)
+
+        if(!result) {
+            return res.status(500).json({message: 'Book not found'})
+        }
+
+        return res.status(200).send({message: "Book is updated Successfully"})
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message: error.message})
+    }
+})
+
+//route for delete a specific book 
+app.delete('/book/:id' , async (req , res) => {
+    try {
+        const {id} = req.params
+        const result = await Book.findByIdAndDelete(id);
+        if (!result) {
+            return res.status(404).json({message: "Book not found"})
+        }
+        return res.status(200).send({message: "Book is deleted successfully"})
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message: error.message})
     }
 })
 
